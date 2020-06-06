934a6aa715db260b38f1e5f67c481dd317ac351d
katsumi-hamaguchi <raikaraika7722@gmail.com>
2020-06-06 14:24:41 +0900

    会員関連カラム、機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  resources :users
+  root 'homes#top'
+  get 'users/event/:id' => 'users#event',as:"event_user"
+  get 'users/withdrawal/:id' => 'users#withdrawal',as:"withdrawal" 
+  put 'users/retire/:id' => 'users#update_user', as: "retire"
+  get 'users/change_pass/:id' => 'users#change_pass',as:"change_pass" 
+  put 'pass/change/:id' => 'users#update_pass', as: "change"
+
 end
